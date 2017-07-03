76121b3d4f58d7c96004a0f14fa958c0f528e77f
iAdanos <iadanos@yandex.com>
2017-07-03 23:10:11 +0300

    Конструкторы
    Конструктор случайного множества.
    Конструктор заданного множества.
    Изменён стиль записи методов в cpp файлах.

--- src/bitWordOR.cpp
+++ src/bitWordOR.cpp
@@ -4,7 +4,7 @@
 
 #include <src/bitWord.h>
 
-set set::operator | (const set & right) const {
+set set :: operator | (const set & right) const {
 
     set result;
     result.word = word | right.word;
